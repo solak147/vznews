e8913104e5686f083493281cca024473e05af1a4
solak1 <rohanasdf12@gmail.com>
2023-04-19 00:24:47 +0800

    訊息

---
+++ pages/message/[id].vue
@@ -0,0 +1,117 @@
+<template>
+  <section>
+    <NavBar title="x先生" />
+
+    <div class="msg-div">
+      <van-space class="msg-right">
+        <div class="speech-bubble">i am text</div>
+        <van-image
+          round
+          width="4rem"
+          height="4rem"
+          src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+        />
+      </van-space>
+    </div>
+
+    <div class="msg-div">
+      <van-space class="msg-left">
+        <van-image
+          round
+          width="4rem"
+          height="4rem"
+          src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+          class="img-left"
+        />
+        <div class="b-f">i am text</div>
+      </van-space>
+    </div>
+  </section>
+</template>
+
+<style lang="less" scoped>
+.speech-bubble {
+  display: inline-block;
+  position: relative;
+  background: #6bf023;
+  border-radius: 0.4em;
+
+  max-width: 30rem;
+  padding: 1.4rem 1rem;
+  margin-top: 5rem;
+  margin-right: 2rem;
+  text-align: center;
+  color: black;
+  font-size: 1.5rem;
+  text-shadow: 0 -0.05em 0.1em rgba(0, 0, 0, 0.3);
+}
+
+.speech-bubble:after {
+  content: '';
+  position: absolute;
+  right: 0;
+  top: 50%;
+  width: 0;
+  height: 0;
+  border: 13px solid transparent;
+  border-left-color: #6bf023;
+  border-right: 0;
+  border-top: 0;
+  margin-top: -6.5px;
+  margin-right: -13px;
+}
+
+.b-f {
+  display: inline-block;
+  position: relative;
+  background: #6bf023;
+  border-radius: 0.4em;
+
+  max-width: 30rem;
+  padding: 1.4rem 1rem;
+  margin-top: 5rem;
+  margin-left: 2rem;
+  text-align: center;
+  color: black;
+  font-size: 1.5rem;
+  text-shadow: 0 -0.05em 0.1em rgba(0, 0, 0, 0.3);
+}
+
+.b-f:after {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 50%;
+  width: 0;
+  height: 0;
+  border: 13px solid transparent;
+  border-right-color: #6bf023;
+  border-left: 0;
+  border-top: 0;
+  margin-top: -6.5px;
+  margin-left: -13px;
+}
+
+.msg-right {
+  float: right;
+
+  .van-image {
+    margin-right: 3rem;
+    margin-top: 3rem;
+  }
+}
+
+.msg-left {
+  float: left;
+
+  .van-image {
+    margin-left: 3rem;
+    margin-top: 3rem;
+  }
+}
+
+.msg-div {
+  height: 8rem;
+  width: 100vw;
+}
+</style>
---
+++ pages/message/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <section>
+    <NavBar title="我的訊息" />
+
+    <van-list
+      v-model:loading="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-swipe-cell v-for="item in list" :key="item">
+        <van-cell @click="navigateTo('/casem/1')">
+          <template #icon>
+            <van-image
+              round
+              width="4rem"
+              height="4rem"
+              src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+            />
+          </template>
+          <template #title> <label>尋找演員</label> <span>5分鐘前</span> </template>
+          <template #label>
+            <van-text-ellipsis rows="2" :content="text" />
+
+            <van-divider :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }" />
+          </template>
+        </van-cell>
+        <template #right>
+          <van-button square type="danger" text="删除" class="r-list-btn" />
+          <van-button square type="primary" text="收藏" class="r-list-btn" />
+        </template>
+      </van-swipe-cell>
+    </van-list>
+  </section>
+</template>
+
+<script setup>
+const text =
+  'Vant 是一个轻量、可定制的移动端组件库，于 2017 年开源。目前 Vant 官方提供了 Vue 2 版本、Vue 3 版本和微信小程序版本，并由社区团队维护 React 版本和支付宝小程序版本。Vant 是一个轻量、可定制的移动端组件库，于 2017 年开源。目前 Vant 官方提供了 Vue 2 版本、Vue 3 版本和微信小程序版本，并由社区团队维护 React 版本和支付宝小程序版本。'
+const list = ref([])
+const loading = ref(false)
+const finished = ref(false)
+
+const onLoad = () => {
+  // 异步更新数据
+  // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+  setTimeout(() => {
+    for (let i = 0; i < 10; i++) {
+      list.value.push(list.value.length + 1)
+    }
+
+    // 加载状态结束
+    loading.value = false
+
+    // 数据全部加载完成
+    if (list.value.length >= 40) {
+      finished.value = true
+    }
+  }, 1000)
+}
+</script>
+
+<style lang="less" scoped>
+:deep(.van-cell__title) {
+  margin-left: 1rem;
+
+  span {
+    color: #969799;
+    float: right;
+  }
+}
+
+.r-list-btn {
+  height: 100%;
+}
+</style>
